91e34352b423e22a01affb97e638e990587704ac
GigiFanariu <139012939+GigiFanariu@users.noreply.github.com>
2023-07-29 11:56:10 +0200

    completato

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println(2 <= 2 && !true);
+
+        System.out.println(!false && 3 > 2);
+
+        System.out.println(!(!false || true));
+
+        System.out.println(6 > 6 ^ !(true && true));
+    }
+}
